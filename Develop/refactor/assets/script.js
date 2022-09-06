0ef15262a56c58a13b4f1c2ca1acea506a7c6107
Brevenn <Brevenjh@gmail.com>
2022-09-06 18:57:36 -0500

    added function for password length

--- Develop/refactor/assets/script.js
+++ Develop/refactor/assets/script.js
@@ -4,11 +4,17 @@ let areNumbers
 let areSpecialCharacters
 
 const passwordPopUp = () => {
+  passChars = parseInt(prompt('How many characters would you like in your password?'))
   isLowerCase = confirm('wanna add lowercase letters?')
   isUpperCase = confirm('WANNA ADD UPPERCASES TO THE MIX?')
   areNumbers = confirm('h0w ab0ut s0me numb3rs t0 g0 w1th?')
   areSpecialCharacters = confirm('~~make your password special with special characters?!~~')
     console.log(isLowerCase, isUpperCase, areNumbers, areSpecialCharacters)
+
+  if(isNaN(passChars) = true || passChars < 8 || passChars > 128 ) {
+    alert('The password must be between 8 and 128 characters')
+      passwordPopUp()
+  }
 }
 
 
